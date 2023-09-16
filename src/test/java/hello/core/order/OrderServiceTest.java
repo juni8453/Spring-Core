b83b455b8ea514d7e9e3881d166043987961297c
ByeoungJoon Jeon <juni8453@naver.com>
2023-09-16 13:43:46 +0900

    사용하지 않는 import 문 정리

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -6,7 +6,6 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
