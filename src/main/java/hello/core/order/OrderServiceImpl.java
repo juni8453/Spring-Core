cfe36511c6f20660d22a377dddf669f57100958c
ByeoungJoon Jeon <juni8453@naver.com>
2023-09-16 13:16:43 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
   private final MemberRepository memberRepository = new MemoryMemberRepository();
-  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+  // 새로운 할인 정책으로 변경하면서 OCP, DIP 를 지키려면 ? -> 인터페이스만 ? -> NPE 발생
+  // 즉, 외부에서 내부에 구현체를 넣어줘야한다.
+  private DiscountPolicy discountPolicy;
 
   // 주문 생성 요청 -> 할인을 적용해 새로운 주문 생성 후 반환
   @Override
