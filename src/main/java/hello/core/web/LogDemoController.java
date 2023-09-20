6eb746d39c42ae6fc918dca3b682aff5675ce98d
ByeoungJoon Jeon <juni8453@naver.com>
2023-09-20 17:47:10 +0900

    스코프와 Proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package hello.core.web;
 import hello.core.common.MyLoger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
   private final LogDemoServie logDemoServie;
-  private final ObjectProvider<MyLoger> myLogerProvider;
+  private final MyLoger myLoger;
 
   @GetMapping("/log")
   @ResponseBody
   public String logDemo(HttpServletRequest request) {
     String requestUrl = request.getRequestURI();
-    MyLoger myLoger = myLogerProvider.getObject();
     myLoger.setRequestUrl(requestUrl);
 
     myLoger.log("controller test");
--- src/main/java/hello/core/web/LogDemoServie.java
+++ src/main/java/hello/core/web/LogDemoServie.java
@@ -2,17 +2,15 @@ package hello.core.web;
 
 import hello.core.common.MyLoger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoServie {
 
-  private final ObjectProvider<MyLoger> myLogerProvider;
+  private final MyLoger myLoger;
 
   public void logic(String id) {
-    MyLoger myLoger = myLogerProvider.getObject();
     myLoger.log("service id = " + id);
   }
 }
