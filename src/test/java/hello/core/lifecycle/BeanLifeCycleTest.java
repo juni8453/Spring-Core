fdea48d7ace0a44390822f7d387bf91de1fe324f
ByeoungJoon Jeon <juni8453@naver.com>
2023-09-20 13:53:16 +0900

    빈 생명주기 콜백 시

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+  @Test
+  public void lifeCycleTest() {
+    AnnotationConfigApplicationContext container = new AnnotationConfigApplicationContext(
+        LifeCycleConfig.class);
+
+    NetworkClient client = container.getBean(NetworkClient.class);
+    container.close();
+  }
+
+  @Configuration
+  static class LifeCycleConfig {
+
+    @Bean
+    public NetworkClient networkClient() {
+      // 수정자 주입
+      NetworkClient networkClient = new NetworkClient();
+      networkClient.setUrl("http://hell-spring.dev");
+      return networkClient;
+    }
+  }
+}
