cbebc6f643345659ff10b05485d6d6d4cbdbb6ef
ByeoungJoon Jeon <juni8453@naver.com>
2023-09-20 14:01:24 +0900

    초기화 콜백, 소멸전 콜백을 지원하는 인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,28 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url);
+  }
+
+  // 의존관계 주입이 끝나면 호출하는 초기화 콜백
+  // 즉, 의존관계 주입 이후 객체 데이터 세팅 시 사용
+  @Override
+  public void afterPropertiesSet() throws Exception {
     connect();
     call("초기화 연결 메세지");
+  }
+
+  // Spring DI Container 가 종료되기 직전 호출하는 소멸전 콜백
+  @Override
+  public void destroy() throws Exception {
+    System.out.println("close: " + url);
   }
 
   public void setUrl(String url) {
@@ -21,10 +36,5 @@ public class NetworkClient {
 
   public void call(String message) {
     System.out.println("call: " + url + " message = " + message);
-  }
-
-  // Service 종료시 호출
-  public void disconnect() {
-    System.out.println("close: " + url);
   }
 }
